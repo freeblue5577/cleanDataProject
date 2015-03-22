f91a8ed01a9d18c8f1d3c4c91e4c260f8993730f
wenwenxu <wenwenxu@umich.edu>
2015-03-22 13:02:47 -0700

    first read in

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,8 @@
 ## for data cleaning course project
 ## wenwen xu
 
+feature_list <- read.table("/Users/fan_y_r/wenwen_project/cleanDataProject/UCI_HAR_Dataset/feature.txt", sep="")
+
+x_train <- read.table("/Users/fan_y_r/wenwen_project/cleanDataProject/UCI_HAR_Dataset/train/X_train.txt", sep="")
+
+
