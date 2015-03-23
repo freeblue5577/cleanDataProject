03c9f3cfe8b0e111f4ad664dfa22643fdd4e4603
wenwenxu <wenwenxu@umich.edu>
2015-03-22 17:10:18 -0700

    submit version with writing tidy_data.txt

--- run_analysis.R
+++ run_analysis.R
@@ -10,6 +10,15 @@ run_analysis <- function() {
   y_train_dir <- paste(cur_dir, "/wenwen_project/cleanDataProject/UCI_HAR_Dataset/train/y_train.txt", sep="")
   y_test_dir <- paste(cur_dir, "/wenwen_project/cleanDataProject/UCI_HAR_Dataset/test/y_test.txt", sep="")
   activity_list_dir <- paste(cur_dir, "/wenwen_project/cleanDataProject/UCI_HAR_Dataset/activity_labels.txt", sep="")
+  write_dir <- paste(cur_dir, "/wenwen_project/cleanDataProject/", sep="")
+  
+  subject_train_dir <- paste(cur_dir, "/wenwen_project/cleanDataProject/UCI_HAR_Dataset/train/subject_train.txt", sep="")
+  subject_test_dir <- paste(cur_dir, "/wenwen_project/cleanDataProject/UCI_HAR_Dataset/test/subject_test.txt", sep="")
+  
+  subject_train <- read.table(subject_train_dir, sep="")
+  subject_test <- read.table(subject_test_dir, sep="")
+  
+  subject_merge <- rbind(subject_train, subject_test)
   
   feature_list <- read.table(feature_list_dir, sep="")
   
@@ -33,9 +42,10 @@ run_analysis <- function() {
   }
   
   final_merge <- cbind(y_convertActName, x_merge)
+  final_merge <- cbind(subject_merge, final_merge)
   
   # attach standard devation to create a new table
-  std_table <- matrix(nrow=2, ncol=dim(x_merge)[2])
+  std_table <- as.table(matrix(nrow=2, ncol=dim(x_merge)[2]))
   rownames(std_table) <- (c("mean", "std"))
   # names(std_table) <- names(final_merge)
   # colnames(std_table)[1] <- colnames(final_merge)[1]
@@ -45,14 +55,21 @@ run_analysis <- function() {
     
     std_table[1, i] <- mean(x_merge[,i])
     std_table[2, i] <- sd(x_merge[,i])
-    #colnames(std_table)[i] <- colnames(as.matrix(x_merge))[i]
+    colnames(std_table)[i] <- as.character(feature_list[i,2])
   }
   
-  std_merge <- rbind(x_merge_copy, std_table)
+  # std_merge <- rbind(x_merge_copy, std_table)
   #temp_merge <- rbind(x_merge, std_table)
   # list(final_merge, std_merge)
-  View(final_merge)
-  View(std_merge)
+  #View(final_merge)
+  #View(std_merge)
+  
+  
+  # call function to print step 5 of final tidy data
+  write_tidy_table(final_merge, write_dir)
+  
+  # write.table(std_table, file=write_dir, row.names=FALSE)
+  final_merge
 }
 
 add_activityNames <- function(actNum_table, actName_table) {
@@ -63,3 +80,10 @@ add_activityNames <- function(actNum_table, actName_table) {
   convertName_table
 }
 
+write_tidy_table <- function (merge_all, dir) {
+  myMelt <- melt(merge_all, id=c("V1", "y_convertActName"), measure.vars=colnames(qq)[3:563])
+  tidy_table <- dcast(myMelt, formula=V1+y_convertActName ~ variable, mean)
+  file_path <- paste(dir, "tidy_data.txt", sep="")
+  write.table(tidy_table, file_path, row.name=FALSE)
+  
+}
